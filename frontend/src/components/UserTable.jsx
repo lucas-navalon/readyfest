f3188900adb58b3c75845a2ff82e6cea0ff4d8d5
Otavio Hipolito <hipolitotata30@gmail.com>
2026-04-05 09:36:05 -0300

    Remove outra sessão do codigo

--- frontend/src/components/UserTable.jsx
+++ frontend/src/components/UserTable.jsx
@@ -30,26 +30,6 @@ export default function UserTable({ editingId, loading, users, onDelete, onEdit 
               </tr>
             </thead>
             <tbody>
-              {users.map((user) => (
-                <tr key={user.id} className={editingId === user.id ? 'row-highlight' : ''}>
-                  <td>{user.id}</td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{formatDate(user.created_at)}</td>
-                  <td className="actions">
-                    <button className="ghost-button" type="button" onClick={() => onEdit(user)}>
-                      Editar
-                    </button>
-                    <button
-                      className="danger-button"
-                      type="button"
-                      onClick={() => onDelete(user.id)}
-                    >
-                      Excluir
-                    </button>
-                  </td>
-                </tr>
-              ))}
             </tbody>
           </table>
         </div>
